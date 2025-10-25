a624d72d839e4211b8cb4be9ebcabbcb13178af6
vexalus <91033522+amingrii@users.noreply.github.com>
2025-10-25 22:33:22 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+// Simple demo: secret number multiplied by 2
+const secretNumber = 42;
+
+// Simple calculation
+const result = secretNumber * 2;
+
+console.log("Input:", secretNumber);
+console.log("Result:", result);
